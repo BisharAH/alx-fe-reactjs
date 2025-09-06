67eb079399d29c3e78092346e8bab2d13ce85929
Bisharahmed <bisharedu@gmail.com>
2025-09-06 13:49:04 +0300

    first

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <main style={{backgroundColor: "blue", color: '#ced7e2ff'}}>
+    <p style={{ lineHeight: '1.6', marginTop: '10px' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent
