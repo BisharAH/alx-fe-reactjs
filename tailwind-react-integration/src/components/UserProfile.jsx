5e22d2a0a91e0a0003d647e2f152fb9839348e3e
Bisharahmed <bisharedu@gmail.com>
2025-09-27 14:48:29 +0300

    main

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-      <img src="https://cdn.pixabay.com/photo/2025/02/21/10/33/cat-9421720_1280.jpg" alt="User" className="w-36 h-36 rounded-full mx-auto" />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+      <img src="https://cdn.pixabay.com/photo/2025/02/21/10/33/cat-9421720_1280.jpg" alt="User" className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto" />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-sm md:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
