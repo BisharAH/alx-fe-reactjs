8a95e7eed8d62fa6459488f067490978bd06487a
Bisharahmed <bisharedu@gmail.com>
2025-09-27 12:34:57 +0300

    main

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,7 @@ import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div className="bg-slate-200 min-h-screen flex items-center justify-center">
+    <div>
       <UserProfile />
     </div>
   );
