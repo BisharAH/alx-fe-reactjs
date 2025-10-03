1e376b0e966ecc1d9e580436ae2fc9996f004275
Bisharahmed <bisharedu@gmail.com>
2025-10-03 14:55:32 +0300

    main

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// import HomePage from "./components/HomePage"
+// import RecipeDetail from "./components/RecipeDetail"
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/recipe/:id" element={<RecipeDetail />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import RecipeDetail from "./components/RecipeDetail"
+import AddRecipeForm from "./components/AddRecipeForm"
 
 function App() {
   return (
@@ -8,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/add" element={<AddRecipeForm />} />
       </Routes>
     </Router>
   )
