d91265fa6e1d16a8a870a622b30cc7eb3d38ec34
Bisharahmed <bisharedu@gmail.com>
2025-10-05 12:46:42 +0300

    main

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 // 👇 Import from the new package and add autoprefixer
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   css: {
     postcss: {
       plugins: [
