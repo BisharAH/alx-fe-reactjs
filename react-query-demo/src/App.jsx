82c6a01aeb2f54a0e7c09c1fbb6c609983c78868
Bisharahmed <bisharedu@gmail.com>
2025-10-12 16:41:00 +0300

    main

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import PostsComponent from './PostsComponent';
 import { ReactQueryDevtools } from 'react-query-devtools';
 import './App.css';
 
+// 1. Create the query client here
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <PostsComponent />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </div>
+    // 2. Wrap your components with the QueryClientProvider here
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <PostsComponent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
